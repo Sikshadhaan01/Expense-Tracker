4589c93245993ba8ce40a1ed081706cd635a3f80
prasheethamukesh <prasheethas@gmail.com>
2024-04-16 11:50:46 +0530

    images

---
+++ lib/services/user_service.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:dio/dio.dart';
+
+class UserService {
+  var baseUrl = dotenv.get("API_URL");
+  Dio dio = Dio();
+
+  signup(jsonObj) async {
+    var url = baseUrl + "/users/sign-up";
+    var encode = jsonEncode(jsonObj);
+    var response = await dio.post(url, data: encode);
+    return response.data;
+  }
+
+  login(jsonObj) async {
+    var url = baseUrl + "/users/login-user";
+    var encode = jsonEncode(jsonObj);
+    var response = await dio.post(url, data: encode);
+    return response.data;
+  }
+}
